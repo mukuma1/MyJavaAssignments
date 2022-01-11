a64a6f3453d3f94d6fffcc539c24ba8265e4a81d
mukuma <hallomamatha@gmail.com>
2022-01-11 02:45:05 -0500

    loop changed

--- src/scannerLoop/waterTank.java
+++ src/scannerLoop/waterTank.java
@@ -6,21 +6,23 @@ public class waterTank {
 
 	public static void main(String[] args) {
 		/*
-		 * Assuming the current level of water tank as empty (0) or a whole number which is a
-		 * multiple of 10
+		 * Assuming the current level of water tank as empty (0) or a whole number which
+		 * is a multiple of 10
 		 **/
 		Scanner sc = new Scanner(System.in);
 		int tank = 0;
 		int bucket = 10;
+		int counter = 0;
 		System.out.println("Enter the Current Level of Water in the Tank");
 		tank = sc.nextInt();
-		for (int i = 0; i <= 10; i++) {
+		do {
 			if (tank == 100) {
-				System.out.println("Tank is Full. " + i + " Buckets of water was needed to fill the tank");
+				System.out.println("Tank is Full. " + counter + " Buckets of water was needed to fill the tank");
 				break;
 			}
 			tank = tank + bucket;
-		}
+			counter++;
+		} while (counter <= 10);
 	}
 
 }
