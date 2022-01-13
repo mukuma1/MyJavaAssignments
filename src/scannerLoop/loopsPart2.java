57a1335678a8726b41d5f38f02f39ee044218e76
mukuma <hallomamatha@gmail.com>
2022-01-13 14:26:11 -0500

    Decrementing loop Initiated

--- src/scannerLoop/loopsPart2.java
+++ src/scannerLoop/loopsPart2.java
@@ -8,7 +8,7 @@ public class loopsPart2 {
 		Scanner sc = new Scanner(System.in);
 		String userid = "";
 		String password = "";
-		for (int i = 1; i <= 3; i++) {
+		for (int i = 3; i > 0; i--) {
 			System.out.println("Enter User Id");
 			userid = sc.next();
 			System.out.println("Enter Password");
@@ -16,12 +16,11 @@ public class loopsPart2 {
 			if (userid.equals("pivotAdmin") && password.equals("Admin123")) {
 				System.out.println("You are Logged in to the application");
 				break;
+			} else if (i == 1) {
+				System.out.println("Maximum attempts of incoorect usename or password. Account locked");
+				break;
 			} else
 				System.out.println("Incorrect Username or password. Try again");
-			if (i == 3) {
-				System.out.println("Account locked");
-				break;
-			}
 		}
 	}
 }
