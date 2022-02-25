7f647339dc25431433a51408091d2467735928d4
mukuma <hallomamatha@gmail.com>
2022-02-24 20:45:11 -0500

    Codes for String in build method

--- src/stringInbuildMethods/Part3.java
+++ src/stringInbuildMethods/Part3.java
@@ -1,12 +1,12 @@
 package stringInbuildMethods;
 
 public class Part3 {
-
+//City name starting with 'T' will be replaced by Mississauga
 	public static void main(String[] args) {
 		String[] cityNames = { "Toronto", "Ottawa", "Brampton" };
 		for (int i = 0; i < cityNames.length; i++) {
 			if (cityNames[i].charAt(0) == 'T') {
-				cityNames[i]="Mississauga";
+				cityNames[i] = "Mississauga";
 			}
 		}
 		for (int i = 0; i < cityNames.length; i++) {
